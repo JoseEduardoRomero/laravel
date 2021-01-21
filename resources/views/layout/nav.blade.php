2a9c7ffb11e1da8f2a561c473d2aff987b01679d
Eduardo Romero <lalin03032000@gmail.com>
2021-01-20 18:22:54 -0600

    Tinker

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -2,7 +2,7 @@
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link {{Route::is('home') ? 'active' : ''}}" href="/">Home</a>
+            <a class="nav-link {{request()->is('/') ? 'active' : ''}}" href="/">Home</a>
         </li>
     <li class="nav-item">
         <a class="nav-link {{Route::is('create') ? 'active' : ''}}"  href="/create">Create</a>
